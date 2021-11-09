4caef022a5ac7328fbc90e80b8e23e64e32b6099
boris ya <boris.yarmoulnik@gmail.com>
2021-11-09 13:47:57 +0300

    feat: add bio section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import { Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react"
+import NextLink from "next/link"
+import { Button, Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react"
 import Section from "../components/section"
 import Paragraph from "../components/paragraph"
+import { ChevronRightIcon } from "@chakra-ui/icons"
+import { BioSection, BioYear  } from "../components/bio"
 
 const Page = () => {
   return (
@@ -40,8 +43,53 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
+
         <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Paragraph>
+
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+
+        <BioSection>
+          <BioYear>1984</BioYear>
+          Born in Town, Country
+        </BioSection>
+
+        <BioSection>
+          <BioYear>2005</BioYear>
+          Completed the MAsters&apos;s Program in the Graduate School of Information Science at IT Institute
+        </BioSection>
+
+        <BioSection>
+          <BioYear>2005</BioYear>
+          Worked at Google Ireland
+        </BioSection>
+
+        <BioSection>
+          <BioYear>2010 to present</BioYear>
+          Works as freelance
+        </BioSection>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          I ❤️
+        </Heading>
+
+        <Paragraph>
+          Art, Misic, Drawing, Cycling, Playing Drums, Photography, Leica, Running.
         </Paragraph>
       </Section>
     </Container>
