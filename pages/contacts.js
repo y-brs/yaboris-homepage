6a62b06f0e96b78532a49682ed22a72faf92e8a7
boris ya <boris.yarmoulnik@gmail.com>
2021-11-10 16:36:24 +0300

    fix: contacts page

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,4 +1,4 @@
-import { Container, Heading } from "@chakra-ui/react"
+import { Container, Stack, Text, Heading } from "@chakra-ui/react"
 import Layout from "../components/layouts/article"
 
 const Contacts = () => (
@@ -8,10 +8,20 @@ const Contacts = () => (
         Contacts
       </Heading>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p>urabitur lectus justo, consequat non sem in, mattis luctus nisi.</p>
-      <p>Vivamus quis laoreet leo, a varius nisl.</p>
-      <p>Fusce a ipsum a odio feugiat facilisis.</p>
+      <Stack spacing={3} mb={5}>
+        <Text fontSize="xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </Text>
+        <Text>
+          Urabitur lectus justo, consequat non sem in, mattis luctus nisi.
+        </Text>
+        <Text>
+          Vivamus quis laoreet leo, a varius nisl.
+        </Text>
+        <Text>
+          Fusce a ipsum a odio feugiat facilisis.
+        </Text>
+      </Stack>
     </Container>
   </Layout>
 )
